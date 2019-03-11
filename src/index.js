ffe4f9143e6fcc96f11a74583a7315d53c503ce6
ktucalvin <ktucalvin@gmail.com>
2019-03-11 13:40:06 -0600

    Add recipe presets

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ProductionCalculator from './components.js'
 import Dolls from './dolls.js'
+import { PresetSelect, standardPresets, heavyPresets } from './presets.js'
 import './index.css'
 
 $(function () {
@@ -20,12 +21,15 @@ $(function () {
     sum: 120,
     server: 'EN'
   }
-  let calculator
+  let calculator, presetSelect
+  let superPreset = standardPresets
 
   ReactDOM.render(
-    <ProductionCalculator dolls={Dolls} recipe={recipe} ref={calc => { calculator = calc }} />,
+    <ProductionCalculator dolls={Dolls} recipe={recipe} ref={r => { calculator = r }} />,
     document.getElementById('root')
   )
+
+  ReactDOM.render(<PresetSelect ref={r => { presetSelect = r }} />, document.getElementById('preset-select'))
 
   function updateDisplay (ingredient) {
     let val = recipe[ingredient] + ''
@@ -48,6 +52,22 @@ $(function () {
     setTimeout(() => $(this).blur(), 150)
   })
 
+  $('#server-id').change(function (e) {
+    recipe.server = $(this).find(':selected').val()
+    calculator.setState(recipe)
+  })
+
+  $('#preset-select').change(function (e) {
+    const preset = superPreset[$(this).find(':selected').val()].preset
+    recipe.manpower = preset[0]
+    recipe.ammunition = preset[1]
+    recipe.rations = preset[2]
+    recipe.parts = preset[3]
+    recipe.sum = recipe.manpower + recipe.ammunition + recipe.rations + recipe.parts
+    calculator.setState(recipe);
+    ['manpower', 'ammunition', 'rations', 'parts'].map(str => updateDisplay(str))
+  })
+
   $('#toggle-heavy').change(function () {
     if (this.checked) {
       $('.heavy').show()
@@ -57,6 +77,8 @@ $(function () {
       $('th.HG').hide()
       recipe.min = recipe.manpower = recipe.ammunition = recipe.rations = recipe.parts = 1000
       recipe.max = 9999
+      superPreset = heavyPresets
+      presetSelect.setState({ presets: heavyPresets })
     } else {
       $('.heavy').hide()
       $('th.HG').show()
@@ -64,14 +86,11 @@ $(function () {
       $('#recipe').removeClass('heavy-style')
       recipe.min = recipe.manpower = recipe.ammunition = recipe.rations = recipe.parts = 30
       recipe.max = 999
+      superPreset = standardPresets
+      presetSelect.setState({ presets: standardPresets })
     }
     ['manpower', 'ammunition', 'rations', 'parts'].map(str => updateDisplay(str))
     recipe.sum = recipe.manpower + recipe.ammunition + recipe.rations + recipe.parts
-    calculator.setState(recipe)
-  })
-
-  $('#server-id').change(function (e) {
-    recipe.server = $(this).find(':selected').val()
     calculator.setState(recipe)
   })
 
---
+++ src/presets.js
@@ -0,0 +1,59 @@
+'use strict'
+import React, { Component } from 'react'
+// Manpower, Ammo, Rations, Parts
+const standardPresets = {
+  hgset1: { preset: [30, 30, 30, 30], text: 'HG Set #1' },
+  hgset2: { preset: [130, 130, 130, 130], text: 'HG Set #2' },
+  beginner: { preset: [430, 430, 430, 130], text: 'Beginner Recommended' },
+  hgMica: { preset: [130, 130, 130, 30], text: 'HG MICA Recommended' },
+  smgCheap: { preset: [400, 400, 91, 30], text: 'SMG Cheap' },
+  smgVoodoo: { preset: [430, 430, 130, 230], text: 'SMG Voodoo' },
+  smgMica: { preset: [400, 400, 100, 200], text: 'SMG MICA Recommended' },
+  arCheap: { preset: [91, 400, 400, 30], text: 'AR Cheap' },
+  arVoodoo: { preset: [130, 430, 430, 230], text: 'AR Voodoo' },
+  g11Voodoo: { preset: [97, 404, 404, 133], text: 'G11 Voodoo' },
+  arMica: { preset: [100, 400, 400, 200], text: 'AR MICA Recommended' },
+  rfCheap: { preset: [400, 91, 400, 30], text: 'RF Cheap' },
+  rfVoodoo: { preset: [430, 130, 430, 230], text: 'RF Voodoo' },
+  rfMica: { preset: [400, 100, 400, 200], text: 'RF MICA Recommended' },
+  mgset1: { preset: [400, 600, 30, 300], text: 'MG Set #1' },
+  mgset2: { preset: [600, 600, 100, 400], text: 'MG Set #2' },
+  mgVoodoo: { preset: [730, 630, 130, 430], text: 'MG Voodoo' },
+  mgMica: { preset: [800, 800, 100, 400], text: 'MG MICA Recommended' }
+}
+
+const heavyPresets = {
+  g11heavy: { preset: [1000, 1000, 1000, 1000], text: 'G11/Cheap' },
+  smgMin: { preset: [4000, 4000, 1000, 1000], text: 'SMG Minimum' },
+  arMin: { preset: [1000, 4000, 4000, 1000], text: 'AR Minimum' },
+  rfset1: { preset: [3000, 1000, 3000, 1000], text: 'RF Set #1' },
+  rfset2: { preset: [4000, 1000, 4000, 1000], text: 'RF Set #2' },
+  mgset1: { preset: [4000, 6000, 1000, 3000], text: 'MG Set #1' },
+  mgset2: { preset: [6000, 6000, 1000, 3000], text: 'MG Set #2' },
+  sgset1: { preset: [4000, 1000, 6000, 3000], text: 'SG Set #1' },
+  sgset2: { preset: [6000, 1000, 6000, 4000], text: 'SG Set #2' },
+  voodoo: { preset: [6000, 2000, 6000, 4000], text: 'SG Voodoo (recommended)' },
+  all: { preset: [6000, 6000, 6000, 4000], text: 'All (not recommended!)' }
+}
+
+export class PresetSelect extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { presets: standardPresets }
+  }
+  render () {
+    let options = []
+    for (const preset in this.state.presets) {
+      options.push(<option key={preset} value={preset}>{this.state.presets[preset].text}</option>)
+    }
+    return (
+      <>
+        <label htmlFor='recipe-preset'>Recipe Preset: </label>
+        <select id='recipe-preset' defaultValue={options[0].props.children} key={Math.random() * 1000}>
+          {options}
+        </select>
+      </>)
+  }
+}
+
+export { standardPresets, heavyPresets }
