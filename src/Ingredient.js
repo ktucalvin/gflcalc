2fe708cfd902d914d56aca012120fce1ff7b6267
ktucalvin <ktucalvin@gmail.com>
2019-08-25 21:45:09 -0600

    Convert to functional components

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,32 +1,31 @@
 'use strict'
-import React, { Component } from 'react'
+import React from 'react'
 
-export default
-class Ingredient extends Component {
-  render () {
-    const gridTemplateColumns = this.props.ingredient >= 1000 ? 'repeat(4, 1fr)' : 'repeat(3, 1fr)'
-    const isHeavy = this.props.ingredient >= 1000
-    return (
-      <div class='ingredient'>
-        <div class='ingredient-name'>
-          <img src={`${this.props.name}.png`} alt={`${this.props.name} icon`} />
-          <span>{this.props.name}</span>
-        </div>
-        <div class={`recipe-input ${this.props.name}`} style={{ gridTemplateColumns }} >
-          {isHeavy && <button class='up' aria-label={`${this.props.name} plus one thousand`} onClick={() => this.props.updateRecipe(1000)} />}
-          <button class='up' aria-label={`${this.props.name} plus one hundred`} onClick={() => this.props.updateRecipe(100)} />
-          <button class='up' aria-label={`${this.props.name} plus ten`} onClick={() => this.props.updateRecipe(10)} />
-          <button class='up' aria-label={`${this.props.name} plus one`} onClick={() => this.props.updateRecipe(1)} />
-          {isHeavy && <p>{Math.floor(this.props.ingredient / 1000 % 10)}</p>}
-          <p>{Math.floor(this.props.ingredient / 100 % 10)}</p>
-          <p>{Math.floor(this.props.ingredient / 10 % 10)}</p>
-          <p>{Math.floor(this.props.ingredient % 10)}</p>
-          {isHeavy && <button class='down' aria-label={`${this.props.name} minus one thousand`} onClick={() => this.props.updateRecipe(-1000)} />}
-          <button class='down' aria-label={`${this.props.name} minus 100`} onClick={() => this.props.updateRecipe(-100)} />
-          <button class='down' aria-label={`${this.props.name} minus 10`} onClick={() => this.props.updateRecipe(-10)} />
-          <button class='down' aria-label={`${this.props.name} minus 1`} onClick={() => this.props.updateRecipe(-1)} />
-        </div>
+const Ingredient = ({ ingredient, name, updateRecipe }) => {
+  const gridTemplateColumns = ingredient >= 1000 ? 'repeat(4, 1fr)' : 'repeat(3, 1fr)'
+  const isHeavy = ingredient >= 1000
+  return (
+    <div class='ingredient'>
+      <div class='ingredient-name'>
+        <img src={`${name}.png`} alt={`${name} icon`} />
+        <span>{name}</span>
       </div>
-    )
-  }
+      <div class={`recipe-input ${name}`} style={{ gridTemplateColumns }} >
+        {isHeavy && <button class='up' aria-label={`${name} plus one thousand`} onClick={() => updateRecipe(1000)} />}
+        <button class='up' aria-label={`${name} plus one hundred`} onClick={() => updateRecipe(100)} />
+        <button class='up' aria-label={`${name} plus ten`} onClick={() => updateRecipe(10)} />
+        <button class='up' aria-label={`${name} plus one`} onClick={() => updateRecipe(1)} />
+        {isHeavy && <p>{Math.floor(ingredient / 1000 % 10)}</p>}
+        <p>{Math.floor(ingredient / 100 % 10)}</p>
+        <p>{Math.floor(ingredient / 10 % 10)}</p>
+        <p>{Math.floor(ingredient % 10)}</p>
+        {isHeavy && <button class='down' aria-label={`${name} minus one thousand`} onClick={() => updateRecipe(-1000)} />}
+        <button class='down' aria-label={`${name} minus 100`} onClick={() => updateRecipe(-100)} />
+        <button class='down' aria-label={`${name} minus 10`} onClick={() => updateRecipe(-10)} />
+        <button class='down' aria-label={`${name} minus 1`} onClick={() => updateRecipe(-1)} />
+      </div>
+    </div>
+  )
 }
+
+export default Ingredient
--- src/ProductionCalculator.js
+++ src/ProductionCalculator.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Dolls, Servers } from './dolls.js'
 
-export default
-class ProductionCalculator extends Component {
-  render () {
-    return (
-      <>
-        {this.props.recipe.sum < 4000 && <Rarity rarity={2} dolls={Dolls.twoStar} recipe={this.props.recipe} />}
-        <Rarity rarity={3} dolls={Dolls.threeStar} recipe={this.props.recipe} />
-        <Rarity rarity={4} dolls={Dolls.fourStar} recipe={this.props.recipe} />
-        <Rarity rarity={5} dolls={Dolls.fiveStar} recipe={this.props.recipe} />
-      </>
-    )
-  }
-}
+const ProductionCalculator = ({ recipe }) => (
+  <>
+    {recipe.sum < 4000 && <Rarity rarity={2} dolls={Dolls.twoStar} recipe={recipe} />}
+    <Rarity rarity={3} dolls={Dolls.threeStar} recipe={recipe} />
+    <Rarity rarity={4} dolls={Dolls.fourStar} recipe={recipe} />
+    <Rarity rarity={5} dolls={Dolls.fiveStar} recipe={recipe} />
+  </>
+)
 
 const rarityClassNames = {
   '3': 'three-star',
@@ -21,54 +16,44 @@ const rarityClassNames = {
   '5': 'five-star'
 }
 
-class Rarity extends Component {
-  render () {
-    const dolls = this.props.dolls
-    const recipe = this.props.recipe
-    return (
-      <tr id={rarityClassNames[this.props.rarity]}>
-        <td className='rarity'>{this.props.rarity}★</td>
-        { recipe.sum < 4000 && <Category dolls={dolls.hg} recipe={this.props.recipe} />}
-        <Category dolls={dolls.smg} recipe={this.props.recipe} />
-        <Category dolls={dolls.ar} recipe={this.props.recipe} />
-        <Category dolls={dolls.rf} recipe={this.props.recipe} />
-        <Category dolls={dolls.mg} recipe={this.props.recipe} />
-        {recipe.sum >= 4000 && <Category dolls={dolls.sg} recipe={this.props.recipe} />}
-      </tr>
-    )
-  }
-}
+const Rarity = ({ recipe, dolls, rarity }) => (
+  <tr id={rarityClassNames[rarity]}>
+    <td className='rarity'>{rarity}★</td>
+    { recipe.sum < 4000 && <Category dolls={dolls.hg} recipe={recipe} />}
+    <Category dolls={dolls.smg} recipe={recipe} />
+    <Category dolls={dolls.ar} recipe={recipe} />
+    <Category dolls={dolls.rf} recipe={recipe} />
+    <Category dolls={dolls.mg} recipe={recipe} />
+    {recipe.sum >= 4000 && <Category dolls={dolls.sg} recipe={recipe} />}
+  </tr>
+)
 
-class Category extends Component {
-  render () {
-    const recipe = this.props.recipe
-    let hidden = 0
-    const result = this.props.dolls.map(doll => {
-      let classes = ''
-      if (doll.unsure && ((recipe.sum < 4000 ? doll.unsure[0] : doll.unsure[1]))) {
-        classes += ' unsure '
-      }
-      if (verifyRecipe(recipe, doll, doll.availability)) {
-        return <Doll key={doll.nameEN} doll={doll} classes={classes} server={recipe.server} />
-      } else if (recipe.showAll && verifyRecipe(recipe, doll, undefined)) { // check again against all servers
-        classes += ' unavailable '
-      } else {
-        hidden++
-        classes += ' flat '
-      }
-      return <Doll key={doll.nameEN} doll={doll} classes={classes} server={recipe.server} />
-    })
-
-    if (hidden !== this.props.dolls.length) {
-      return <td><ul>{result}</ul></td>
-    } else {
-      return <td><ul><None server={recipe.server} />{result}</ul></td>
+const Category = ({ recipe, dolls }) => {
+  let hidden = 0
+  const result = dolls.map(doll => {
+    let classes = ''
+    if (doll.unsure && ((recipe.sum < 4000 ? doll.unsure[0] : doll.unsure[1]))) {
+      classes += 'unsure '
     }
+    if (verifyRecipe(recipe, doll, doll.availability)) {
+      return <Doll key={doll.nameEN} doll={doll} classes={classes} server={recipe.server} />
+    } else if (recipe.showAll && verifyRecipe(recipe, doll, undefined)) { // check again against all servers
+      classes += 'unavailable '
+    } else {
+      hidden++
+      classes += 'flat'
+    }
+    return <Doll key={doll.nameEN} doll={doll} classes={classes} server={recipe.server} />
+  })
+
+  if (hidden !== dolls.length) {
+    return <td><ul>{result}</ul></td>
+  } else {
+    return <td><ul><None />{result}</ul></td>
   }
 }
 
-// Note this method doesn't work if the doll's
-// name is in a different language to its server
+// Note this method doesn't work if the doll's name is in a different language to its server
 const serverToIso = {
   EN: 'en',
   CN: 'zh-CN',
@@ -77,34 +62,29 @@ const serverToIso = {
   JP: 'ja'
 }
 
-class Doll extends Component {
-  render () {
-    const doll = this.props.doll
-    let name = doll['name' + this.props.server]
-    if (!name) { name = doll.nameEN }
-    const lang = name !== doll.nameEN ? serverToIso[this.props.server] : 'en'
-    return (
-      <li className={this.props.classes}>
-        <span lang={lang}>{name}</span>
-        <span>{doll.time}</span>
-      </li>
-    )
-  }
+const Doll = ({ doll, server, classes }) => {
+  let name = doll['name' + server]
+  if (!name) { name = doll.nameEN }
+  const lang = name !== doll.nameEN ? serverToIso[server] : 'en'
+  return (
+    <li className={classes}>
+      <span lang={lang}>{name}</span>
+      <span>{doll.time}</span>
+    </li>
+  )
 }
 
-class None extends Component {
-  render () {
-    return (<li><span>NONE</span><span>-:--:--</span></li>)
-  }
-}
+const None = () => (<li><span>NONE</span><span>-:--:--</span></li>)
 
 function verifyRecipe (recipe, doll, availability) {
   const requirements = recipe.sum < 4000 ? doll.standard : doll.heavy
-  if (!requirements) return false
-  return recipe.manpower >= requirements[0] &&
+  return requirements &&
+         recipe.manpower >= requirements[0] &&
          recipe.ammunition >= requirements[1] &&
          recipe.rations >= requirements[2] &&
          recipe.parts >= requirements[3] &&
          (requirements[4] ? requirements[4](recipe.sum) : true) && // check any recipe sum conditions
          (availability === undefined || Servers[recipe.server] & availability) // check server availability
 }
+
+export default ProductionCalculator
